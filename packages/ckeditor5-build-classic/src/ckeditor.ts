273774aae3c157273a0e1f56f15e68cc98615936
Vu Minh Hieu <hieuvm@topcv.vn>
2023-05-11 13:30:18 +0700

    add placeholder feature

--- packages/ckeditor5-build-classic/src/ckeditor.ts
+++ packages/ckeditor5-build-classic/src/ckeditor.ts
@@ -9,22 +9,22 @@ import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classicedi
 import { Essentials } from '@ckeditor/ckeditor5-essentials';
 import { UploadAdapter } from '@ckeditor/ckeditor5-adapter-ckfinder';
 import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
-import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
+import { Bold, Italic, Underline, Strikethrough } from '@ckeditor/ckeditor5-basic-styles';
 import { CKBox } from '@ckeditor/ckeditor5-ckbox';
 import { CKFinder } from '@ckeditor/ckeditor5-ckfinder';
 import { EasyImage } from '@ckeditor/ckeditor5-easy-image';
 import { Heading } from '@ckeditor/ckeditor5-heading';
 import { Image, ImageCaption, ImageStyle, ImageToolbar, ImageUpload, PictureEditing } from '@ckeditor/ckeditor5-image';
-import { Indent } from '@ckeditor/ckeditor5-indent';
 import { Link } from '@ckeditor/ckeditor5-link';
 import { List } from '@ckeditor/ckeditor5-list';
-import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
 import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
-import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
 import { TextTransformation } from '@ckeditor/ckeditor5-typing';
 import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
+import Placeholder from './placeholder/placeholder';
+
+import Font from '@ckeditor/ckeditor5-font/src/font';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 
 export default class ClassicEditor extends ClassicEditorBase {
 	public static override builtinPlugins = [
@@ -32,8 +32,9 @@ export default class ClassicEditor extends ClassicEditorBase {
 		UploadAdapter,
 		Autoformat,
 		Bold,
+		Underline,
+		Strikethrough,
 		Italic,
-		BlockQuote,
 		CKBox,
 		CKFinder,
 		CloudServices,
@@ -44,26 +45,25 @@ export default class ClassicEditor extends ClassicEditorBase {
 		ImageStyle,
 		ImageToolbar,
 		ImageUpload,
-		Indent,
 		Link,
 		List,
-		MediaEmbed,
 		Paragraph,
 		PasteFromOffice,
 		PictureEditing,
-		Table,
-		TableToolbar,
-		TextTransformation
+		TextTransformation,
+		Placeholder,
+		Font,
+		Alignment,
 	];
 
 	public static override defaultConfig = {
 		toolbar: {
 			items: [
 				'undo', 'redo',
-				'|', 'heading',
-				'|', 'bold', 'italic',
-				'|', 'link', 'uploadImage', 'insertTable', 'blockQuote', 'mediaEmbed',
-				'|', 'bulletedList', 'numberedList', 'outdent', 'indent'
+				'|', 'heading', 'fontFamily',
+				'|', 'bold', 'italic', 'underline', 'strikethrough', 'fontColor',
+				'|', 'bulletedList', 'numberedList', 'alignment',
+				'|', 'uploadImage', 'link',
 			]
 		},
 		image: {
@@ -74,13 +74,6 @@ export default class ClassicEditor extends ClassicEditorBase {
 				'|',
 				'toggleImageCaption',
 				'imageTextAlternative'
-			]
-		},
-		table: {
-			contentToolbar: [
-				'tableColumn',
-				'tableRow',
-				'mergeTableCells'
 			]
 		},
 		// This value must be kept in sync with the language defined in webpack.config.js.
---
+++ packages/ckeditor5-build-classic/src/placeholder/placeholderui.js
@@ -0,0 +1,6 @@
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+
+export default class PlaceholderUI extends Plugin {
+    init() {
+	}
+}
